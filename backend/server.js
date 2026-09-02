e854d6985e698abeddbc5d3db26e870080ff3bd9
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2026-09-02 10:48:03 +0530

    Fix production CORS

--- backend/server.js
+++ backend/server.js
@@ -19,32 +19,11 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      console.log("Incoming CORS Origin:", origin);
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-
+    origin: "https://bitmax-assesment.vercel.app",
     credentials: true,
-
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-      "PATCH",
-      "OPTIONS",
-    ],
-
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
-  })
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
 );
 
 app.use(express.json());
